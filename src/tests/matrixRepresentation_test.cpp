311e6e3a8ba1ddbf920bbcbbaa3e8c057471e9a1
Pedro Magalhaes <pfsmagalhaes@gmail.com>
2018-09-07 15:41:09 -0300

    Ajustando o teste da matrixRepresentation

--- src/tests/matrixRepresentation_test.cpp
+++ src/tests/matrixRepresentation_test.cpp
@@ -2,12 +2,56 @@
 #include <iostream>
 
 
+// g++ -std=c++14 ./../matrixRepresentation.cpp  matrixRepresentation_test.cpp -o teste
+
+void printRepresentartionMatrix( matrixRepresentation & M , int n) {
+    for (size_t i = 0; i < n; i++)
+    {   
+        for (size_t j = 0; j < n; j++)
+        {
+            std::cout<<" "<< M.valueat(i,j);
+        }
+        std::cout<<"\n"<<std::endl;
+    }
+}
+
+void printMatrix( std::vector< std::vector<int> > & M , int n) {
+    for (size_t i = 0; i < n; i++)
+    {   
+        for (size_t j = 0; j < n; j++)
+        {
+            std::cout<<" "<< M[i][j];
+        }
+        std::cout<<"\n"<<std::endl;
+    }
+}
+
+void compareValues( std::vector< std::vector<int> > & M , matrixRepresentation & R ,int n) {
+    for (size_t i = 0; i < n; i++)
+    {   
+        for (size_t j = 0; j < n; j++)
+        {
+            if( M[i][j] != R.valueat(i,j) )
+            {
+                std::cout<< "<<ERROR>>Matrix not equal value at line: "<<i<<" row: "<<j<<" are not equal."<<std::endl;
+                std::cout<< "Matrix : \n"<<std::endl;
+                printMatrix(M,n);
+                std::cout<< "Representation : \n"<<std::endl;
+                printRepresentartionMatrix(R,n);
+                return;
+            }
+        }
+    }
+    std::cout<< "<<SUCCESS>>Matrix and Representation Matrix are equal."<<std::endl;
+}
+
 int main (void) 
 {
-    int myMatrix[5][5];
+    std::vector< std::vector<int> > myMatrix(5,std::vector<int>(5));
     std::string lowerRepresentation = ""; // tem representacao dos zeros da diagonal
     std::string upperRepresentation = ""; // diagonal nao representada
     matrixRepresentation m;
+    
     for (size_t i = 0; i < 5; i++)
     {   
         for (size_t j = i; j < 5; j++)
@@ -20,20 +64,12 @@ int main (void)
             else
             {
                 myMatrix[i][j] = 0;
-            }
-            
-            
+            }            
         }
     }
+    std::cout<<"test matrix :"<<std::endl;
+    printMatrix(myMatrix,5);
 
-    for (size_t i = 0; i < 5; i++)
-    {   
-        for (size_t j = 0; j < 5; j++)
-        {
-            std::cout<<" "<<myMatrix[i][j];
-        }
-        std::cout<<"\n"<<std::endl;
-    }
 
     // criando a string que representa a matriz com a parte superior
     for (size_t i = 0; i < 5; i++) 
@@ -54,35 +90,17 @@ int main (void)
         }
     }
     std::cout<<lowerRepresentation<<std::endl;
-
     m = matrixRepresentation(5,MatrixType::UPPER_ROW);
     m.readMatrixData(upperRepresentation);
+    std::cout<<"Teste com matriz upper row: "<<std::endl;
+    compareValues(myMatrix,m,5);
 
-    m.printVector();
-
-    std::cout<<"\n"<<std::endl;
-    
-    for (size_t i = 0; i < 5; i++)
-    {   
-        for (size_t j = 0; j < 5; j++)
-        {
-            std::cout<<" "<<m.valueat(j,i);
-        }
-        std::cout<<"\n"<<std::endl;
-    }
-    std::cout<<"\n Passada a representacao lower: "<<std::endl;
-
+    // m.printVector();
     m = matrixRepresentation(5,MatrixType::LOWER_DIAG_ROW);
     m.readMatrixData(lowerRepresentation);
+    std::cout<<"Teste com matriz lower diag row: "<<std::endl;
+    compareValues(myMatrix,m,5);
 
-    for (size_t i = 0; i < 5; i++)
-    {   
-        for (size_t j = 0; j < 5; j++)
-        {
-            std::cout<<" "<<m.valueat(j,i);
-        }
-        std::cout<<"\n"<<std::endl;
-    }
 
     return 0;
 }   
